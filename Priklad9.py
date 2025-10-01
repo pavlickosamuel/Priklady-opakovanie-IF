3f147432165bb81a52b6efa3598887f4a3c67f70
pavlickosamuel <pavlickosamuel@gmail.com>
2025-10-01 09:44:33 +0200

    finalna verzia

--- Priklad9.py
+++
@@ -1,8 +0,0 @@
-# Program 9: Kontrola písmena v reťazci
-a = (input("Zadaj retazec: "))
-b = (input("Zadaj pismeno: "))
-print(b in a)
-if b in a:
-    print ("Pismeno sa nachadza v retazci")
-else:
-    print ("Pismeno sa nenachadza v retazci")
